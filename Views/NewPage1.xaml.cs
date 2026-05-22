10bea4c72a6652556626bba110c80c0eee2c9dc9
CarinaMelo <carina.melo.rock@hotmail.com>
2026-05-22 18:26:48 -0300

    comitt

---
+++ Views/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotell.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/SobrePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotell;
+
+public partial class SobrePage : ContentPage
+{
+    public SobrePage()
+    {
+        InitializeComponent();
+    }
+}
